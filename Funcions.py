857e9f96285fd7191a65c0a8bce4757699ddc8d7
Francesc Bernat Bieri Tauler <francescbernatf@gmail.com>
2022-06-07 14:50:09 +0200

    Afegit una funció per representar 2 gràfiques a Funcions.py, Afegit el calcul del potencial de membrana per a cada cas al Treball numeric II

--- Funcions.py
+++ Funcions.py
@@ -258,3 +258,79 @@ def Grafic(x, y, ylabel = "V (mV)", xlabel = "t (ms)", color="blue",
             print('Guardant a directori especificat!')
             path = NovaSubcarpeta(path)
             fig.savefig( path + '\\' + title + '.pdf', dpi = 400)
+
+def VarisGrafics(x, y1, y2, ylabel = "V (mV)", xlabel = "t (ms)", color1="blue", 
+              color2="green", label1 = None, label2 = None, path = None, 
+              guardar = False, title = None):
+    '''
+    Funció per a representar dues funcions en un mateix gràfic.
+
+    Parameters
+    ----------
+    x : ARRAY
+        Valors que representar en l'eix x.
+    y1 / y2 : ARRAY
+        Valors que representar en l'eix y.
+    ylabel : STRING, optional
+        Nom de l'eix y. The default is "V (mV)".
+    xlabel : STRING, optional
+        Nom de l'eix x. The default is "t (ms)".
+    color1 / color2 : STRING or LIST, optional
+        Assignar un valor vàlid per a definir els
+        colors de cada gràfic. The default is "blue" and "green".
+    label1 / label2 : STRING, optional
+        Donar un nom a cada funció.
+    path : STRING, optional
+        Nom de la subcarpeta on guardar les gràfiques. 
+        De no ésser especificat, se guardaran en la mateixa subcarpeta
+        The default is None.
+    guardar : Boolean, optional
+        Si volem guardar les gràfiques generades. The default is None.
+    title : STRING, optional
+        Titol que posar a les gràfiques o nom de les
+        gràfiques guardades. Necesari per poder guardar les gràfiques
+        The default is None.
+
+    Returns
+    -------
+    None.
+
+    '''
+    
+    import matplotlib.pyplot as plt
+    
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=400)
+    
+    ax.plot(x, y1, c = color1, label=label1)
+    ax.plot(x, y2, c = color2, label=label2)
+    ax.tick_params(axis='both', labelsize = 12)
+    
+    ax.set_ylabel(ylabel, fontsize = 14)
+    ax.set_xlabel(xlabel, fontsize = 14)
+    
+    if title and not guardar:
+        ax.set_title(title, fontsize = 20)
+    
+    ax.yaxis.get_ticklocs(minor=True)
+    ax.yaxis.set_ticks_position('both'); 
+    ax.xaxis.set_ticks_position('both')
+    
+    if any([label1, label2]):
+        ax.legend()
+    
+    ax.minorticks_on()
+    plt.tight_layout()
+    plt.show()
+    
+    
+    if guardar and title:
+        import re
+        title = re.sub('[. =-]','_', title.replace('\n', ''))
+        if path is None:
+            print('Guardant...')
+            fig.savefig( title + '.pdf', dpi = 400)
+            
+        else:
+            print('Guardant a directori especificat!')
+            path = NovaSubcarpeta(path)
+            fig.savefig( path + '\\' + title + '.pdf', dpi = 400)
